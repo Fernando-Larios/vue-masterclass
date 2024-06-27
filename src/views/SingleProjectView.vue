913239d88b1a86468c9578c17d8afbff52dc1a37
Fernando Larios <lariosly2@gmail.com>
2024-06-27 13:54:47 -0600

    wildcar params on vue router

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template lang="">
+  <div>
+    <h1>New {{ route.params?.id }}</h1>
+  </div>
+</template>
