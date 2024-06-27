8e4faf7cd8faf128ef740b6b8ca608450ce1fd58
Fernando Larios <lariosly2@gmail.com>
2024-06-27 13:28:40 -0600

    dynamic import from vue routes

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template lang="">
+  <div>
+    <h1>Projects</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+  </div>
+</template>
+
+<style lang=""></style>
